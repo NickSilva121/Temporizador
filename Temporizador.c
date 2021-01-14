65f0076ec5b0f0d673be527062b386481a090f11
Nicolas Pereira <77083485+NickSilva121@users.noreply.github.com>
2021-01-13 21:03:31 -0300

    Update Temporizador.c

--- Temporizador.c
+++ Temporizador.c
@@ -4,10 +4,7 @@
 
 int main ()
 {
-    int hora;
-    int min;
-    int seg;
-    int ts = 1;
+    int hora, min, seg, ts = 1;
     
     printf("Digite a hora:");
     scanf("%i", &hora);
@@ -18,35 +15,47 @@ int main ()
     
     if (seg > 60) {
         min += (seg / 60);
-        seg = seg - 60;
+        seg -= 60;    
     }
     else if(min > 60){
         hora += (min / 60);
+        min = min - 60;
     }
-
-    for (hora, min, seg; seg--;){
-        printf("%i:%i:%i\n", hora, min, seg);
-        sleep(ts);
-        system("clear");
+    else if((min > 0)&&(seg == 0)){
+        min--;
+        seg = 60;
         
-        if ((min > 0)&&(seg == 0)){
-            min--;
-            seg = 59;
+        for (hora, min, seg; seg--;){
             printf("%i:%i:%i\n", hora, min, seg);
             sleep(ts);
             system("clear");
-        }
-        else if((min == 0)&&(seg == 0)&&(hora > 0)){
-            hora--;
-            seg = 59;
-            min = 59;
-            printf("%i:%i:%i\n", hora, min, seg);
-            sleep(ts);
-            system("clear");
+
+            if((seg == 0)&&(min > 0)){
+                min = 0;
+                seg = 60;
+                printf("%i:%i:%i\n", hora, min, seg);
+                sleep(ts);
+                system("clear");
+            }
+            else if ((min > 0)&&(seg == 0)){
+                min--;
+                seg = 60;
+                printf("%i:%i:%i\n", hora, min, seg);
+                sleep(ts);
+                system("clear");
+            }
+            else if((min == 0)&&(seg == 0)&&(hora > 0)){
+                hora--;
+                seg = 60;
+                min = 60;
+                printf("%i:%i:%i\n", hora, min, seg);
+                sleep(ts);
+                system("clear");
+            }
         }
     }
     
     printf("Tempo esgotado!\n\n");
  
     return 0;
-}
+}    
